8c26320ed15754bc6a32195318741fdd8ec32234
Azim <azimhao@gmail.com>
2023-03-21 22:00:26 +0800

    Add project files.

---
+++ CafeDeTerunjing/Pages/About.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CafeDeTerunjing.Pages
+{
+    public class AboutModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
